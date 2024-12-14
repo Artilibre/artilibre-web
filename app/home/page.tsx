30a8468c65e24e0e2ba672e1b89389e4ae02e9d5
Antoine Arnoult <antoine.arnoult@artilibre.fr>
2024-12-14 20:13:09 +0100

    Make search component

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,10 @@
 import { Twochoices } from "@/components/home/two-choices";
 import { Footer } from "@/components/layout/footer";
 import { NavBar } from "@/components/layout/nav-bar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { SearchIcon } from "lucide-react";
 import Image from "next/image";
 
 const HomePage = async () => {
@@ -9,11 +12,20 @@ const HomePage = async () => {
     <div>
       <NavBar />
       <div className="relative">
-        <Image src="/bg-craftman.jpg" alt="Craftman" width="1366" height="768" objectFit="cover" />
+        <div className="relative h-[35vh] w-full md:h-[50vh] lg:h-[65vh]">
+          <Image src="/bg-craftman.jpg" alt="Craftman" layout="fill" objectFit="cover" />
+        </div>
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="container grid grid-cols-3 px-4 text-white">
             <div className="col-span-2">
-              <span className="text-6xl font-bold">Trouvez l'artisan parfait pour vos travaux.</span>
+              <span className="text-2xl font-bold md:text-4xl lg:text-6xl">Trouvez l'artisan parfait pour vos travaux.</span>
+              <div className="mt-4 flex h-10 space-x-2 md:h-12">
+                <Input placeholder="Rechercher un artisan" className="w-full" />
+                <Button className="h-full">
+                  <SearchIcon className="h-4 w-4" />
+                  <span className="hidden md:inline">Rechercher</span>
+                </Button>
+              </div>
             </div>
           </div>
         </div>
