a3d63ced152b53e82675525694ff351dbf43bb5c
Antoine Arnoult <antoine.arnoult@artilibre.fr>
2024-12-14 15:44:59 +0100

    💄 Add first element of homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,21 @@
 import { Footer } from "@/components/home/footer";
 import { NavBar } from "@/components/home/nav-bar";
+import Image from "next/image";
 
 const HomePage = async () => {
   return (
     <div>
       <NavBar />
-      <div className="m-3 flex flex-col">Welcome home page</div>
+      <div className="relative">
+        <Image src="/bg-craftman.jpg" alt="Craftman" width="1366" height="768" objectFit="cover" />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="container grid grid-cols-3 px-4 text-white">
+            <div className="col-span-2">
+              <span className="text-6xl font-bold">Trouvez l'artisan parfait pour vos travaux.</span>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
