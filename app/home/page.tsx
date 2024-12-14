9f6cc7eb11273bca20248f23899216cbee1481be
Antoine Arnoult <antoine.arnoult@artilibre.fr>
2024-12-14 16:37:02 +0100

    Plug CTAs on hp for craftman and carftman looker

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,7 @@
 import { Footer } from "@/components/home/footer";
 import { NavBar } from "@/components/home/nav-bar";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
 
 const HomePage = async () => {
@@ -16,6 +18,30 @@ const HomePage = async () => {
           </div>
         </div>
       </div>
+
+      <div className="container mx-auto my-3 grid grid-cols-1 gap-0 md:grid-cols-2 md:gap-3">
+        <div className="bg-tertiary flex flex-col p-8 text-center text-white md:rounded">
+          <h1 className="mb-6 text-3xl font-semibold">Vous cherchez un artisan ?</h1>
+          <p className="mb-8 mt-auto opacity-90">
+            Nulla libero purus, vestibulum nec maximus id, suscipit vel enim. Quisque scelerisque consequat lorem vel efficitur. Donec dictum urna vitae urna
+            dapibus condimentum. Cras sed turpis leo.
+          </p>
+          <Button className="self-center">Créer un compte</Button>
+        </div>
+        <div className="flex flex-col bg-primary p-8 text-center text-white md:rounded">
+          <h1 className="mb-6 text-3xl font-semibold">Vous êtes artisan ?</h1>
+          <p className="mb-8 mt-auto opacity-90">
+            Nulla libero purus, vestibulum nec maximus id, suscipit vel enim. Quisque scelerisque consequat lorem vel efficitur. Donec dictum urna vitae urna
+            dapibus condimentum. Cras sed turpis leo.
+          </p>
+          <Button variant="inverse" className="self-center">
+            Créer un compte
+          </Button>
+        </div>
+      </div>
+
+      <Separator className="container mx-auto mt-10" />
+
       <Footer />
     </div>
   );
--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
   const craftmans = ["Plombier", "Chauffagiste", "Electricien", "Carreleur", "Couvreur", "Charpentier", "Macon"];
 
   return (
-    <footer className="w-full bg-white py-12">
+    <footer className="w-full bg-white py-8">
       <div className="container mx-auto px-4">
         <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
           {/* Logo and Description Column */}
